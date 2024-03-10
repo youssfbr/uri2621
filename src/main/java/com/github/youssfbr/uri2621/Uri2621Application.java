cac0802944102928010c2cf21ec88a7a8ee70df2
Alisson Youssf <youssfbr@gmail.com>
2024-03-10 20:08:09 -0300

    Initial commit

---
+++ src/main/java/com/github/youssfbr/uri2621/Uri2621Application.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.uri2621;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uri2621Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uri2621Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/youssfbr/uri2621/Uri2621ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.uri2621;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uri2621ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
